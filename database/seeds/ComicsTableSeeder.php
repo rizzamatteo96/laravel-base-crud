6c4c64add53df7f5a2f40fcfb2909653c4a8346b
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-09 15:25:22 +0200

    add commics seeder reading data from comic.php populate comics table

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comicArray = config('comics');
+        
+        foreach($comicArray as $comic){
+            
+            $newComic = new Comic();
+            
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+
+            $newComic->save();
+
+        }
+    }
+}
