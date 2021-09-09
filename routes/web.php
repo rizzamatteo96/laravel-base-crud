8a64345ce51f1e7918123fdb96824dbd07eacb47
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-09 15:46:18 +0200

    create controller and pages basic setup

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@homepage')->name('home-page');
+
+Route::resource('/comics', 'ComicController');
