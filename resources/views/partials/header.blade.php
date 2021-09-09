b3fb0f6ff8aef78415710941c2863f46f272bf75
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-09 16:03:07 +0200

    add header nav bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,20 @@
 <header>
-  HEADER DI PROVA
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="{{route('home-page')}}">DC Comics</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('home-page')}}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('comics.index')}}">Comics</a>
+          </li>
+        </ul>  
+      </div>
+    </div>
+  </nav>
 </header>
